2918909be714408bcd5aea451285744f61b4fdc0
Matheus Albino <matheusalbino@users.noreply.github.com>
2021-01-27 23:38:51 -0300

    github actions test

--- native/src/epicgames/mod.rs
+++ native/src/epicgames/mod.rs
@@ -1,11 +1,11 @@
+#[cfg(target_os = "windows")]
 mod windows;
 mod item;
 
 use crate::scan::types::Game;
-use std::env;
 
 pub fn games() -> std::io::Result<Vec<Game>> {
-  if env::consts::OS == "windows" {
+  if cfg!(target_os = "windows") {
     return windows::games::list();
   }
 
