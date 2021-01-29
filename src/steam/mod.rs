e73a3efed27bccb09d9b67529f701a193fe88cf1
Matheus Albino <matheusalbino@users.noreply.github.com>
2021-01-29 01:55:30 -0300

    fix build

--- src/steam/mod.rs
+++ src/steam/mod.rs
@@ -1,7 +1,18 @@
 mod acf;
 mod vdf;
-#[cfg(target_os = "windows")]
-mod windows;
 
 #[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
 pub use windows::*;
+
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub use linux::*;
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+pub use macos::*;
+
