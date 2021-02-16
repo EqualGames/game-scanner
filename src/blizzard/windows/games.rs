43c59bc25dd40061ba464938f4568b8078f32bf0
Matheus Albino <matheusalbino@users.noreply.github.com>
2021-02-16 00:58:28 -0300

    fix build errors

--- src/blizzard/windows/games.rs
+++ src/blizzard/windows/games.rs
@@ -5,24 +5,23 @@ use crate::util::registry::get_local_machine_reg_key;
 use std::path::{Path, PathBuf};
 
 pub fn list() -> Result<Vec<Game>> {
-    let launcher_executable =
+    let launcher_info =
         get_local_machine_reg_key("Microsoft\\Windows\\CurrentVersion\\Uninstall\\Battle.net")
             .and_then(|launcher_reg| {
                 launcher_reg
                     .get_value::<String, &str>("DisplayIcon")
                     .map_err(Error::from)
             })
-            .map(PathBuf::from)
-            .map_err(|error| {
-                Error::new(
-                    ErrorKind::LauncherNotFound,
-                    format!(
-                        "Invalid Blizzard path, maybe this launcher is not installed: {}",
-                        error.to_string()
-                    ),
-                )
-            })
-            .unwrap();
+            .map(PathBuf::from);
+
+    if launcher_info.is_err() {
+        return Err(Error::new(
+            ErrorKind::LauncherNotFound,
+            "Invalid Blizzard path, maybe this launcher is not installed",
+        ));
+    }
+
+    let launcher_executable = launcher_info.unwrap();
 
     if !launcher_executable.exists() {
         return Err(Error::new(
