a5425ed5d0774f4fb89216ee4326534290916ee0
Matheus Albino <matheusalbino@users.noreply.github.com>
2021-11-01 19:37:20 -0300

    refactor: update npm index.js

--- node/npm/index.js
+++ node/npm/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
-if (process.platform === 'win32') {
-    module.exports = require("./native/windows.node");
-} else if (process.platform === 'darwin') {
-    module.exports = require("./native/darwin.node");
-} else if (process.platform === 'linux') {
-    module.exports = require("./native/linux.node");
+if (process.arch === 'x64') {
+    if (process.platform === 'win32') {
+        module.exports = require("./native/windows.node");
+    } else if (process.platform === 'darwin') {
+        module.exports = require("./native/darwin.node");
+    } else if (process.platform === 'linux') {
+        module.exports = require("./native/linux.node");
+    } else {
+        throw new Error(`platform (${process.platform}) not supported`);
+    }
 } else {
-    throw new Error('invalid platform');
+    throw new Error(`architecture (${process.arch}) not supported`);
 }
