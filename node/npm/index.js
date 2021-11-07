f8038cfc5e5f3f4e2c13667950b5dfb5e08f82f6
Matheus Albino <matheusalbino@users.noreply.github.com>
2021-11-07 12:52:02 -0300

    refactor: update npm index.js

--- node/npm/index.js
+++ node/npm/index.js
@@ -5,8 +5,6 @@ if (process.arch === 'x64') {
         module.exports = require("./native/windows.node");
     } else if (process.platform === 'darwin') {
         module.exports = require("./native/darwin.node");
-    } else if (process.platform === 'linux') {
-        module.exports = require("./native/linux.node");
     } else {
         throw new Error(`platform (${process.platform}) not supported`);
     }
