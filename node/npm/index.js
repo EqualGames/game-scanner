c3b264d3a21daeb2f97d679aabb2e67b1527e3a7
Equal <6087948+is-equal@users.noreply.github.com>
2022-04-09 11:58:42 -0300

    chore: add support to Linux and arm64 platforms on pipeline and npm package

--- node/npm/index.js
+++ node/npm/index.js
@@ -2,9 +2,21 @@
 
 if (process.arch === 'x64') {
     if (process.platform === 'win32') {
-        module.exports = require("./native/windows.node");
+        module.exports = require("./native/windows-x64.node");
     } else if (process.platform === 'darwin') {
-        module.exports = require("./native/darwin.node");
+        module.exports = require("./native/darwin-x64.node");
+    } else if (process.platform === 'linux') {
+        module.exports = require("./native/linux-x64.node");
+    } else {
+        throw new Error(`platform (${process.platform}) not supported`);
+    }
+} else if (process.arch === 'arm64') {
+    if (process.platform === 'win32') {
+        module.exports = require("./native/windows-arm64.node");
+    } else if (process.platform === 'darwin') {
+        module.exports = require("./native/darwin-arm64.node");
+    } else if (process.platform === 'linux') {
+        module.exports = require("./native/linux-arm64.node");
     } else {
         throw new Error(`platform (${process.platform}) not supported`);
     }
