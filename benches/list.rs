e73a03a091fce9f4d72ff25c5349d96912f43260
Equal <6087948+is-equal@users.noreply.github.com>
2022-07-21 23:07:11 -0300

    refactor: update benches for macOS

--- benches/list.rs
+++ benches/list.rs
@@ -1,54 +1,73 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::*;
 
 fn list(c: &mut Criterion) {
-    c.bench_function("amazon::games", |b| {
+    let mut group = c.benchmark_group("list");
+
+    #[cfg(target_os = "windows")]
+    group.bench_function("amazon::games", |b| {
         b.iter(|| {
             let games = game_scanner::amazon::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("blizzard::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("blizzard::games", |b| {
         b.iter(|| {
             let games = game_scanner::blizzard::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("epicgames::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("epicgames::games", |b| {
         b.iter(|| {
             let games = game_scanner::epicgames::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("gog::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("gog::games", |b| {
         b.iter(|| {
             let games = game_scanner::gog::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("origin::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("origin::games", |b| {
         b.iter(|| {
             let games = game_scanner::origin::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("riotgames::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("riotgames::games", |b| {
         b.iter(|| {
             let games = game_scanner::riotgames::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("steam::games", |b| {
+
+    #[cfg(not(target_os = "linux"))]
+    group.bench_function("steam::games", |b| {
         b.iter(|| {
             let games = game_scanner::steam::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
-    c.bench_function("ubisoft::games", |b| {
+
+    #[cfg(target_os = "windows")]
+    group.bench_function("ubisoft::games", |b| {
         b.iter(|| {
             let games = game_scanner::ubisoft::games().unwrap();
             assert_ne!(games.len(), 0);
         })
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, list);
