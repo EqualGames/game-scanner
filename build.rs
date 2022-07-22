a2a4e8112f23cdbc9075660b2168468c8cce7037
Equal <6087948+is-equal@users.noreply.github.com>
2022-07-21 22:47:57 -0300

    chore: upgrade rust dependencies

--- build.rs
+++ build.rs
@@ -1,13 +1,15 @@
-extern crate prost_build;
-
 use std::path::PathBuf;
 
 fn main() {
     let blizzard_proto_path = PathBuf::from("src").join("blizzard").join("proto");
-    println!("cargo:rerun-if-changed={}", blizzard_proto_path.to_string_lossy());
+    println!(
+        "cargo:rerun-if-changed={}",
+        blizzard_proto_path.to_string_lossy()
+    );
 
     println!("Compiling protos...");
     prost_build::Config::new()
+        .btree_map(&["."])
         .out_dir(&blizzard_proto_path)
         .compile_protos(
             &[blizzard_proto_path.join("product_db.proto")],
